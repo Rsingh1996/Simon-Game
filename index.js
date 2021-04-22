edfcc29d4748eedfd87454b7aebea0fd8cd298d8
Rahul <rahulna1996@gmail.com>
2021-04-22 23:42:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+
+$(document).keypress(function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass(" pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass(" pressed");
+    }, 100);
+}
+
+
+function nextSequence() {
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.random() * 4;
+    randomNumber = Math.floor(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+$(" .btn").click(function () {
+
+    var userChosenColor = this.id;
+
+    userClickedPattern.push(userChosenColor);
+
+    animatePress(userChosenColor);
+
+    playSound(userChosenColor);
+
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass(" pressed");
+    setTimeout(function () {
+        $("#" + currentColour).removeClass(" pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("Wrong");
+        $("h1").text("Game Over, Press Any Key to Restart");
+
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        startOver();
+    }
+}
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
